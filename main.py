e2dcba20182457fda39724f7087da6bc19bd5c20
Racool198 <51079028+Racool198@users.noreply.github.com>
2022-01-19 23:10:16 +1030

    Create main.py

---
+++ main.py
@@ -0,0 +1,6 @@
+# Copyright 2022 Rahul Satheesan fixer of "Dead Cells Run Decider"
+# Disclaimer: This software is not associated with Motion Twin, Evil Empire or anything or anyone outside of Isaac Carabott
+# Licence: This software is free to use, copy, modify, merge, publish, distribute and/or sell without any interference provided that 
+# this text be included with it and it contains the same licence used here.
+
+print("Hello World');
